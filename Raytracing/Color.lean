adeb08939afd6bb0d4d5c47a5d75fcdfc51a8718
misterclayt0n <daviarantes13@gmail.com>
2025-08-07 10:40:39 -0300

    drawing a sphere according to it's normal vectors

--- Raytracing/Color.lean
+++ Raytracing/Color.lean
@@ -30,23 +30,25 @@ def toPPMLine (c : Color) : String :=
   let (r, g, b) := toBytes c
   s!"{r} {g} {b}"
 
-def hit_sphere (center : Point3) (radius : Float) (ray : Ray) : Bool :=
-  let oc           := center - ray.orig
-  let a            := Vec3.dot ray.dir ray.dir
-  let b            := -2.0 * Vec3.dot ray.dir oc
-  let c            := Vec3.dot oc oc - radius^2
-  let discriminant := b^2 - 4.0*a*c
-  discriminant >= 0
+def hit_sphere (center : Point3) (radius : Float) (ray : Ray) : Float :=
+  let oc           := ray.orig - center
+  let a            := ray.dir.lengthSq
+  let half_b       := Vec3.dot oc ray.dir
+  let c            := oc.lengthSq - radius^2
+  let discriminant := half_b^2 - a*c
+  if discriminant < 0 then -1.0 else (-half_b - Float.sqrt discriminant) / a
 
 def ray_color (r : Ray) : Color :=
-  if hit_sphere ⟨0, 0, -1⟩ 0.5 r then ⟨1, 0, 0⟩ else
-  
-  let unit_dir := Vec3.normalize r.dir
-  let a        := 0.5 * (unit_dir.y + 1.0)
-  let white    := Color.mk 1.0 1.0 1.0
-  let blue     := Color.mk 0.5 0.7 1.0
-
-  (1.0 - a) * white + a * blue
+  let t := hit_sphere ⟨0.0, 0.0, -1.0⟩ 0.5 r
+  if t > 0.0 then
+    let n := (r.pointAt t - ⟨0.0, 0.0, -1.0⟩).normalize
+    (0.5 : Float) * mk (n.x + 1) (n.y + 1) (n.z + 1)
+  else
+    let unit_dir := Vec3.normalize r.dir
+    let a        := 0.5 * (unit_dir.y + 1.0)
+    let white    := Color.mk 1.0 1.0 1.0
+    let blue     := Color.mk 0.5 0.7 1.0
+    (1.0 - a) * white + a * blue
 
 end Color
   
